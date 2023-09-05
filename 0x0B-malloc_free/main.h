b7ea1d3dc599157bf9577b5d48e5fbafac452350
Midoban888 <midobansnakebite@gmail.com>
2023-09-06 00:21:34 +0100

    0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,3 @@
-main.h
-
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -10,4 +8,5 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+
 #endif
